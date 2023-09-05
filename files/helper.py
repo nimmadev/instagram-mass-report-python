1c709e474cb6f0145097f103f0fe7854619ed70e
nimmadev <116873964+nimmadev@users.noreply.github.com>
2023-09-05 21:37:59 +0530

    Update helper.py
    updated each function

--- files/helper.py
+++ files/helper.py
@@ -1,21 +1,21 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-def illeagl(page):
-    page.get_by_role("button", name="Sale of illegal or regulated goods chevron").click()
+def illegal(page):
+    page.locator("button").filter(has_text="Sale of illegal or regulated goodschevron").click()
     page.get_by_text("Firearms").click()
     page.get_by_role("button", name="Submit Report").click()
     page.get_by_role("button", name="Close").click()
 
 def scam(page):
-    page.get_by_role("button", name="Scam or fraud chevron").click()
+    page.locator("button").filter(has_text="Scam or fraudchevron").click()
     page.get_by_role("button", name="Close").click()
 
 def pretend_me(page):
-    page.get_by_text("Someone I know").click()
+    page.get_by_text("Someone I follow").click()
     page.get_by_role("button", name="Submit Report").click()
     page.get_by_role("button", name="Close").click()
 
 def pretend_someone(page):
-    page.get_by_role("group").get_by_text("Me").first.click()
+    page.get_by_text("Me", exact=True).click()
     page.get_by_role("button", name="Submit Report").click()
     page.get_by_role("button", name="Close").click()
